e553166dfb1dcf8181e930d595c90ff24a62b17e
ivan_razuvaev <alfredvilgelm@gmail.com>
2023-04-12 19:04:49 +0300

    example005_Condition_if_else

---
+++ example005_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine() ?? "";
+
+if (userName.ToLower() == "настасья")
+{
+    Console.WriteLine("Ура! это же Настасья!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
