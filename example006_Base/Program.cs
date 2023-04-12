31456ef5b7012fb87acde04d3779afea313311d6
ivan_razuvaev <alfredvilgelm@gmail.com>
2023-04-12 23:00:57 +0300

    ex006.1

--- example006_Base/Program.cs
+++ example006_Base/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1670;
+int b = 233;
+int c = 631;
+int d = 81;
+int e = 490;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.Write(max);
